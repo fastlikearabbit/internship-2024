ff12a2c23e75f7af8e9ff86f5c52943d28a75394
Daniel Ghenghea <danielghenghea@Daniels-MacBook-Pro.local>
2024-01-18 13:18:47 +0200

    validated input

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FirstTask
+﻿using System.Text.RegularExpressions;
+
+namespace FirstTask
 {
     enum Color
     {
@@ -10,13 +12,16 @@
     {
         static Color GetSquareColor(string position)
         {
+            if (!Regex.IsMatch(position, "^[A-Ha-h][1-8]$"))
+            {
+                throw new ArgumentOutOfRangeException(position);
+            }
             return (position[0] + position[1]) % 2 == 1 ? Color.White : Color.Black;
-            
         }
         static void Main(string[] args)
         {
             // should return Black
-            Console.WriteLine(GetSquareColor("a1"));
+            Console.WriteLine(GetSquareColor("1"));
             
             // should return White
             Console.WriteLine(GetSquareColor("h7"));
